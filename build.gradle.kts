3daa6457f0f1dad1f2038d946333e5bf781f039e
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-09-23 11:56:20 +0200

    Use fopbot:0.4.0-SNAPSHOT
    - now Robot.setNumberOfCoins(int) is available

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ submit {
 
 repositories {
     mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 
 val publicTest: SourceSet by sourceSets.creating {
@@ -34,7 +35,7 @@ dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
     // JUnit only available in "test" source set (./src/test)
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    implementation ("org.sourcegrade:fopbot:0.3.0")
+    implementation ("org.tudalgo:fopbot:0.4.0-SNAPSHOT")
 }
 
 application {

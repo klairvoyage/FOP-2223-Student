9de27812b36ace77c57dcba8a454f56fc592aed2
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-18 01:53:41 +0200

    remove toString

--- src/main/java/h06/EvaluationResult.java
+++ src/main/java/h06/EvaluationResult.java
@@ -1,23 +1,21 @@
 package h06;
 
+/**
+ * The result of the evaluation of a bracket expression.
+ */
 public record EvaluationResult(
     Type type,
     int nextIndex
 ) {
-    @Override
-    public String toString() {
-        return "EvaluationResult{" +
-            "type=" + type +
-            ", nextIndex=" + nextIndex +
-            '}';
-    }
 
+    /**
+     * An enumeration of constants representing results of a bracket expression evaluation.
+     */
     public enum Type {
         CORRECT,
         INVALID_CHARACTER,
         NO_OPENING_BRACKET,
         NO_CLOSING_BRACKET,
         INVALID_CLOSING_BRACKET
-
     }
 }

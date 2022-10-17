f1afd9c9bcf8a529504a4535f048eaa78c976d1d
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-18 01:53:14 +0200

    fix and add methods

--- src/main/java/h06/StrangeFunctions.java
+++ src/main/java/h06/StrangeFunctions.java
@@ -2,21 +2,84 @@ package h06;
 
 import static org.tudalgo.algoutils.student.Student.crash;
 
-public interface StrangeFunctions {
+/**
+ * A collection of strange functions.
+ */
+public class StrangeFunctions {
 
-    static double[] strangeFunction1(int m, int n) {
+    /**
+     * A strange function.
+     *
+     * @param m a number
+     * @param n another number
+     * @return a mysterious result
+     */
+    public static int strangeFunction1(int m, int n) {
         return crash();
     }
 
-    static double[] strangeFunction2(int m, int n) {
+    /**
+     * Just another strange function.
+     *
+     * @param m just a number
+     * @param n just another number
+     * @return just another mysterious result
+     */
+    public static int strangeFunction2(int m, int n) {
         return crash();
     }
 
-    static double[] understandable1(double m, double n) {
+    /**
+     * Oh, there is a third strange function?
+     *
+     * @param m a number
+     * @param n another number
+     * @return just another mysterious result
+     */
+    public static double understandable1(double m, double n) {
         return crash();
     }
 
-    static double[] understandable2(double m, double n) {
+    /**
+     * How much more strange functions are there?
+     * @param m a number
+     * @param n another number
+     * @return
+     */
+    public static double understandable2(double m, double n) {
+        return crash();
+    }
+
+    /**
+     * A simple function to transform arrays on a strange way.
+     *
+     * @param array the array to transform
+     * @return the transformed array
+     */
+    public static double[] transformArray1(double[] array) {
+        return crash();
+    }
+
+    /**
+     * Another simple function to transform arrays on a strange way.
+     *
+     * @param array the array to transform
+     * @return the transformed array
+     */
+    public static double[] transformArray2(double[] array) {
+        return crash();
+    }
+
+    /**
+     * This function is doing the recursive work!
+     *
+     * @param in the input array
+     * @param out the output array
+     * @param i the index of the current element
+     * @return the transformed array
+     * @see #doTheRecursion(double[], double[], int)
+     */
+    public static double[] doTheRecursion(double[] in, double[] out, int i) {
         return crash();
     }
 }

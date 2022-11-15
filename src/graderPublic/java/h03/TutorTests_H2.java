f43455cf29e07ed142401c2cb873dbb83197601e
Steyer, Nick <nick.steyer@stud.tu-darmstadt.de>
2022-11-15 18:14:28 +0100

    Fix error message for Test H2 testAddToDirectionOfOffspring

--- src/graderPublic/java/h03/TutorTests_H2.java
+++ src/graderPublic/java/h03/TutorTests_H2.java
@@ -224,7 +224,7 @@ public class TutorTests_H2 {
         Context context = contextBuilder()
             .add("offspring", robot)
             .add("directionAccu", directionAccu)
-            .add("directionToBeAdded", directionAccu)
+            .add("directionToBeAdded", directionToBeAdded)
             .build();
         addToDirectionOfOffspringMethod.invoke(instance, directionToBeAdded);
 

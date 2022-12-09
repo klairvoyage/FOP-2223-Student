a9918266c4085795d4dd6164191209d817c21234
klairvoyage <122225342+klairvoyage@users.noreply.github.com>
2022-12-09 23:46:00 +0100

    Meine H06-Abgabe

--- src/main/java/h06/StrangeFunctions.java
+++ src/main/java/h06/StrangeFunctions.java
@@ -15,7 +15,12 @@ public class StrangeFunctions {
      * @return a mysterious result
      */
     public static double strangeFunction1(int m, int n) {
-        return crash(); // TODO: H1.1 - remove if implemented
+        // TODO: H1.1 - remove if implemented
+        if (m<n) return m;
+        else {
+            if (0>(m%n)) return strangeFunction1(m-(m%n), n);
+            else return (double)m/n;
+        }
     }
 
     /**
@@ -26,7 +31,8 @@ public class StrangeFunctions {
      * @return just another mysterious result
      */
     public static double strangeFunction2(int m, int n) {
-        return crash(); // TODO: H1.1 - remove if implemented
+        // TODO: H1.1 - remove if implemented
+        return m<n ? m : 0>(m%n) ? strangeFunction2(m-(m%n), n) : (double)m/n;
     }
 
     /**
@@ -37,7 +43,11 @@ public class StrangeFunctions {
      * @return just another mysterious result
      */
     public static boolean understandable1(double m, double n) {
-        return crash(); // TODO: H1.2 - remove if implemented
+        // TODO: H1.2 - remove if implemented
+        if ((m<=0) && (n<=0)) return true;
+        else if ((m<=0) || (n<=0)) return false;
+        else if (m<n) return understandable1(m-1, m+(2*n));
+        else return understandable1(m, m-n);
     }
 
     /**
@@ -47,7 +57,8 @@ public class StrangeFunctions {
      * @return just another mysterious result
      */
     public static boolean understandable2(double m, double n) {
-        return crash(); // TODO: H1.2 - remove if implemented
+        // TODO: H1.2 - remove if implemented
+        return ((m<=0) && (n<=0)) ? true : ((m<=0) || (n<=0)) ? false : (m<n) ? understandable2(m-1, m+(2*n)) : understandable2(m, m-n);
     }
 
     /**
@@ -56,7 +67,13 @@ public class StrangeFunctions {
      * @param array the array to transform
      */
     public static void transformArray1(double[] array) {
-        crash(); // TODO: H2 - remove if implemented
+        // TODO: H2 - remove if implemented
+        if (array.length==1) array[0]++;
+        else if (array.length>1) {
+            double temp = array[0];
+            for (int i=0;i<array.length-1;i++) array[i] += array[i+1];
+            array[array.length-1] += temp;
+        }
     }
 
     /**
@@ -65,7 +82,13 @@ public class StrangeFunctions {
      * @param array the array to transform
      */
     public static void transformArray2(double[] array) {
-        crash(); // TODO: H2 - remove if implemented
+        // TODO: H2 - remove if implemented
+        if (array.length==1) array[0]++;
+        else if (array.length>1) {
+            double temp = array[0];
+            doTheRecursion(array, 0);
+            array[array.length-1] += temp;
+        }
     }
 
     /**
@@ -75,6 +98,10 @@ public class StrangeFunctions {
      * @param i the index of the current element
      */
     public static void doTheRecursion(double[] array, int i) {
-        crash(); // TODO: H2 - remove if implemented
+        // TODO: H2 - remove if implemented
+        if (i!=array.length-1) {
+            array[i] += array[i+1];
+            doTheRecursion(array, i+1);
+        }
     }
 }

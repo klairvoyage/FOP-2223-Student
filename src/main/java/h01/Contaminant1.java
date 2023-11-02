a8d2e0be711713a61aea673de00a57dca1e917f8
klairvoyage <122225342+klairvoyage@users.noreply.github.com>
2023-11-02 21:00:18 +0100

    H01_2.1)

--- src/main/java/h01/Contaminant1.java
+++ src/main/java/h01/Contaminant1.java
@@ -4,8 +4,12 @@ import fopbot.Direction;
 import fopbot.Robot;
 import fopbot.RobotFamily;
 import h01.template.Contaminant;
+import h01.template.GameConstants;
 import h01.template.TickBased;
+import h01.template.Utils;
 import org.tudalgo.algoutils.student.Student;
+
+import java.util.ArrayList;
 
 /**
  * A {@link Contaminant}-{@link Robot} that moves randomly and contaminates the floor.
@@ -32,6 +36,28 @@ public class Contaminant1 extends Robot implements Contaminant, TickBased {
     @Override
     public void doMove() {
         // TODO: H2.1
-        Student.crash("H2.1 - remove if implemented");
+        if (!hasAnyCoins()) turnOff();
+        else if (!isTurnedOff()) {
+            // puts a random amount of coins (as many as it can) if current field has less than 20
+            for (int i=0; i<Utils.getRandomInteger(GameConstants.CONTAMINANT_ONE_MIN_PUT_COINS,
+                GameConstants.CONTAMINANT_ONE_MAX_PUT_COINS); i++) {
+                if (Utils.getCoinAmount(getX(), getY())<20 && hasAnyCoins()) putCoin();
+            }
+            // collects all viable direction options in a list during a 360-turn
+            ArrayList<Integer> potentialDirections = new ArrayList<>();
+            for (int i=0; i<4; i++) {
+                if (isFrontClear()) potentialDirections.add(getDirection().ordinal());
+                turnLeft();
+            }
+            if (!potentialDirections.isEmpty()) {
+                // chooses a random direction from the list of viable options
+                int nextDirection;
+                do nextDirection = Utils.getRandomInteger(0, 3);
+                while (!potentialDirections.contains(nextDirection));
+                // turns to the chosen direction
+                while (getDirection().ordinal()!=nextDirection) turnLeft();
+            }
+            if (isFrontClear()) move();
+        }
     }
 }
